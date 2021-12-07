e11fb1b2eae5ffe3c2fe6e55e4bdd98170dce72b
aleja <pacadenae@unal.edu.co>
2021-12-07 04:05:36 -0500

    vista administrar miembros grupo v1

--- bUNparche/controllers/grupos.js
+++ bUNparche/controllers/grupos.js
@@ -1,8 +1,8 @@
 const {db} = require("../database/configdb")
 
 const crearGrupo = async (res,req)=>{
-    const {nombre,descripcion,oficial,grCategoria} = req.body;
-    if(!nombre || !descripcion || !grCategoria){
+    const {nombre,descripcion,oficial,grCategoria,id_creador} = req.body;
+    if(!nombre || !descripcion || !grCategoria || !id_creador){
         return res.status(400).json('error en la submission del form');
     }
 
@@ -15,8 +15,13 @@ const crearGrupo = async (res,req)=>{
     .into('CategoriaGrupo')
 	.then(grCat => {
         res.json(grCat[0])
+        console.log(grCat[0])
 	})
 	.catch(err => res.status(400).json('error creando grupo'));
+    db.insert({
+        ID_usuario: id_creador,
+        ID_permiso: 1
+    }).into('PermisosUsuario')
 }
 
 const grCategoriasGet = async (res,req)=>{
