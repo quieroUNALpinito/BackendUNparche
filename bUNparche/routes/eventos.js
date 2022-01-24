4981e7d0d832e3533f52739f211153e45b02babd
Vinnie Mellizo <vgmellizom@unal.edu.co>
2022-01-24 00:06:10 -0500

    Added two new POST routes to filter by groups and changed listarEventos to POST

--- bUNparche/routes/eventos.js
+++ bUNparche/routes/eventos.js
@@ -4,17 +4,19 @@ const eventController = require('../controllers/eventos')
 const router = Router()
 
 router.get('/tiposEvento', eventController.tiposEvento)
+router.post('/gruposDeUSuario', eventController.gruposDeUsuario)
 router.post('/crearEvento', eventController.crearEvento)
 
 router.post('/confirmarAsistenciaEvento', eventController.confirmarAsistencia)
 
-router.get('/listarEventos',eventController.listarEventos)
+router.post('/listarEventos',eventController.listarEventos)
 router.post('/listarEventosByHour',eventController.listarEventosByHour)
 router.post('/verEvento',eventController.verEvento)
 router.post('/confirmarAsistenciaEvento',eventController.confirmarAsistencia)
 router.post('/desconfirmarAsistenciaEvento',eventController.desconfirmarAsistencia)
 router.post('/consultarAsistenciaUsuarioEvento',eventController.consultarAsistenciaUsuarioEvento)
 router.post('/listarEventosByType',eventController.listarEventosByType)
+router.post('/listarEventosByGroup',eventController.listarEventosByGroup)
 router.post('/listarEventosByLocation',eventController.listarEventosByLocation)
 
 module.exports = router
